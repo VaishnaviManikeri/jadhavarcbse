9f86ba7e23dbcce699755cd0a0b4afef8c920a55
Vaishnavi Manikeri <vaishnavimanikeri@gmail.com>
2026-02-11 17:02:41 +0530

    Fix blog controller 500 error

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,34 +1,79 @@
 const Blog = require("../models/Blog");
 
-/* CREATE */
+/* ================= CREATE ================= */
+
 exports.createBlog = async (req, res) => {
   try {
-    const blog = new Blog(req.body);
-    await blog.save();
-    res.json(blog);
-  } catch (err) {
-    res.status(500).json(err);
+    const { title, description } = req.body;
+
+    if (!title || !description) {
+      return res.status(400).json({
+        message: "Title and description are required",
+      });
+    }
+
+    const blog = new Blog({
+      title,
+      description,
+      image: req.body.image || "",
+    });
+
+    const savedBlog = await blog.save();
+
+    res.status(201).json(savedBlog);
+
+  } catch (error) {
+    console.error("Create Blog Error:", error);
+    res.status(500).json({ message: "Server error while creating blog" });
   }
 };
 
-/* READ */
+/* ================= GET ALL ================= */
+
 exports.getBlogs = async (req, res) => {
-  const blogs = await Blog.find().sort({ createdAt: -1 });
-  res.json(blogs);
+  try {
+    const blogs = await Blog.find().sort({ createdAt: -1 });
+    res.json(blogs);
+  } catch (error) {
+    console.error("Get Blogs Error:", error);
+    res.status(500).json({ message: "Server error while fetching blogs" });
+  }
 };
 
-/* UPDATE */
+/* ================= UPDATE ================= */
+
 exports.updateBlog = async (req, res) => {
-  const updated = await Blog.findByIdAndUpdate(
-    req.params.id,
-    req.body,
-    { new: true }
-  );
-  res.json(updated);
+  try {
+    const updated = await Blog.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+
+    if (!updated) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+
+    res.json(updated);
+  } catch (error) {
+    console.error("Update Blog Error:", error);
+    res.status(500).json({ message: "Server error while updating blog" });
+  }
 };
 
-/* DELETE */
+/* ================= DELETE ================= */
+
 exports.deleteBlog = async (req, res) => {
-  await Blog.findByIdAndDelete(req.params.id);
-  res.json({ message: "Deleted" });
+  try {
+    const deleted = await Blog.findByIdAndDelete(req.params.id);
+
+    if (!deleted) {
+      return res.status(404).json({ message: "Blog not found" });
+    }
+
+    res.json({ message: "Blog deleted successfully" });
+  } catch (error) {
+    console.error("Delete Blog Error:", error);
+    res.status(500).json({ message: "Server error while deleting blog" });
+  }
 };
