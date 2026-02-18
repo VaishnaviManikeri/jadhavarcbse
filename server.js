5769c64c9c25efb743f7b0aafc43af851fd0207d
Vaishnavi Manikeri <vaishnavimanikeri@gmail.com>
2026-02-18 18:38:24 +0530

    updated backend in server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ if (!fs.existsSync(uploadsDir)) {
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000", "https://jadhavarcbse.onrender.com"],
+    origin: ["https://jadhavarcbse.com", "http://localhost:3000", "https://jadhavarcbse.onrender.com"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
